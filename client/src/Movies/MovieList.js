6caefe250c939f16426605fae6e7e4aa50ea27f2
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-14 15:44:54 -0500

    added functionality to the movie cards in the movie list

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -13,8 +14,13 @@ export default function MovieList(props) {
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
+  const history = useHistory();
+  const routeDescription = () => {
+    history.push(`/movies/${props.movie.id}`);
+  };
+
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={routeDescription}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
